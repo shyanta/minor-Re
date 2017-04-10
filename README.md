406dc00c42d4c298c78a96fa35b73d9467aa7cfb
Shyanta Vleugel <Shyanta Vleugel>
2017-04-10 14:07:28 +0200

    Integrated Express and required Socket.io

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# minor-Real Time Web 
+# Minor - Real Time Web
+
+## Introduction
+In this project I'm going to build a real time web application. I'm going to make a
+website that visually represents a real-time data source. Nowadays people expect websites
+to be real-time. Think about al the social media platforms. When someone posts something, you want
+to see it immediately. Just like whatsapp, when someone sends you a message, you want to see it
+directly, you don't want to wait around for multiple seconds.
+
+This is why real-time is very popular right now. It's fast and more importantly, it's what people
+expect, and as a frontend developer this is what should trigger you.
+
+## Sources
---
+++ app.js
@@ -0,0 +1,27 @@
+// NPM requirements
+var express = require('express');
+var app = express();
+var path = require('path');
+var ejs = require('ejs');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// declare app Routers
+var homeRouter = require('./routes/home');
+
+// Set view engine to .ejs and tell app where these files are placed
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// Tell express which static files to serve
+app.use(express.static('public'));
+
+// Declare routes for the app to listen to
+app.get('/', homeRouter);
+
+app.get('/*', function(req, res){
+	res.render('404');
+});
+
+http.listen(3000, function(req, res){
+	console.log('App is running at localhost:3000')
+});

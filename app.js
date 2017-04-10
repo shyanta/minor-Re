0083a1bb0a101eb9416d59df346da4d7ce083dec
Shyanta Vleugel <Shyanta Vleugel>
2017-04-10 15:08:15 +0200

    Add Socket.io Basic Chat App

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ app.get('/*', function(req, res){
 	res.render('404');
 });
 
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+		console.log('message: ' + msg);
+	});
+});
+
 http.listen(3000, function(req, res){
 	console.log('App is running at localhost:3000')
 });
